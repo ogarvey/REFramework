f80f11d83d39d0af7176847cf1ca59751a9ae69e
OGarvey <ogarvey@gmail.com>
2025-12-15 01:45:48 +0000

    add using

--- csharp-api/REFCoreDeps/Compiler.cs
+++ csharp-api/REFCoreDeps/Compiler.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Metadata;
+using REFrameworkNET;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Text;
